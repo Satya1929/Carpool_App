32d4553a2174b0b8b57ee836dd8c2019ab9a6709
Satya1929 <spswain2003.org@gmail.com>
2024-10-22 19:45:08 +0530

    new text

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
+# import streamlit as st
+
+# def main():
+#     # st.title("Travel Partner Finder")
+#     st.header("🧑‍🤝‍🧑 Seeking travel partner? Look no further!! (Diwali version)")
+
+#     st.write(
+#         """
+#         Simply fill out this Google form with your preferences and get ready to connect with potential travel partners. 
+
+#         Once you've submitted your form, you'll have access to a spreadsheet where you can explore other people's preferences in google speadsheet below or simply put Date in next page of App and get connected !!
+
+#         Don't miss out on this opportunity! Start now by filling out the form. 🚀 
+#         """
+#     )
+
+#     # Google Form and Spreadsheet links
+#     st.markdown(
+#         """
+#         - **Google Form Link 🔗:** [Click](<https://docs.google.com/forms/d/e/1FAIpQLScmQSHtkn3IhlAm7NJFA_ujO2vSKNP_lS7i4g2VQG1nWNT-2g/viewform>)
+#         - **Spreadsheet 📕:** [Click](https://docs.google.com/spreadsheets/u/1/d/1fbEmtrmVu9heYMfL5P1a9qJ5q0xT3tFj3y2TFVEiGIk/edit?usp=sharing)
+#         """
+#     )
+
+#     st.write(
+#         """
+#         Improving upon feedback dm to Me....!!! 👀✌🏻 (7735416363)
+
+#         📍 IMPORTANT: You can edit your preference, details. A link to edit the response will be emailed to you .
+
+#         📍 YouTube - [Click](https://www.youtube.com/watch?v=HJjtxDpOIow) (Carpool Guide)
+
+#         """
+#     )
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
 import streamlit as st
 
 def main():
     # st.title("Travel Partner Finder")
-    st.header("🧑‍🤝‍🧑 Seeking travel partner? Look no further!! (Diwali version)")
+    st.header("Seeking travel partner?🧑‍🤝‍🧑 Look no further!! (Diwali version)")
 
     st.write(
         """
-        Simply fill out this Google form with your preferences and get ready to connect with potential travel partners. 
+        Welcome to the Travel Match App! 🚗
 
-        Once you've submitted your form, you'll have access to a spreadsheet where you can explore other people's preferences in google speadsheet below or simply put Date in next page of App and get connected !!
+        Your go-to platform for finding the perfect travel partner based on shared preferences. Just fill out a quick form and you're all set!
 
         Don't miss out on this opportunity! Start now by filling out the form. 🚀 
         """
@@ -17,18 +68,23 @@ def main():
     # Google Form and Spreadsheet links
     st.markdown(
         """
-        - **Google Form Link 🔗:** [Click](<https://docs.google.com/forms/d/e/1FAIpQLScmQSHtkn3IhlAm7NJFA_ujO2vSKNP_lS7i4g2VQG1nWNT-2g/viewform>)
-        - **Spreadsheet 📕:** [Click](https://docs.google.com/spreadsheets/u/1/d/1fbEmtrmVu9heYMfL5P1a9qJ5q0xT3tFj3y2TFVEiGIk/edit?usp=sharing)
+        📍Pages Available:
+        - *🏡 Home -> Form:* Enter your preferences [Here](<https://docs.google.com/forms/d/e/1FAIpQLScmQSHtkn3IhlAm7NJFA_ujO2vSKNP_lS7i4g2VQG1nWNT-2g/viewform>).
+        - *🏡 Home -> Spreadsheet:* Access traveler info to connect with potential matches [Here](<https://docs.google.com/forms/d/e/1FAIpQLScmQSHtkn3IhlAm7NJFA_ujO2vSKNP_lS7i4g2VQG1nWNT-2g/viewform>).
+        - *🔎 Match By Date:* Select your preferred date to match with 100+ travelers. Plus, plan your time and destination with our analysis of fellow travelers.
+        - *📊 All Dates Summary:* View the overall preferences of everyone out there!
         """
     )
 
     st.write(
         """
-        Improving upon feedback dm to Me....!!! 👀✌🏻 (7735416363)
+        Start your journey and make carpooling hassle-free! 🎉
 
-        📍 IMPORTANT: You can edit your preference, details. A link to edit the response will be emailed to you .
+        Got questions or feedback? Feel free to DM me anytime! 👀✌🏻 (7735416363)
 
-        📍 YouTube - [Click](https://www.youtube.com/watch?v=HJjtxDpOIow) (Carpool Guide)
+        📍 IMPORTANT: A link to edit Your Response will be emailed to you 
+    
+        📍 YouTube: [Carpool Guide](https://www.youtube.com/watch?v=HJjtxDpOIow) 
 
         """
     )
