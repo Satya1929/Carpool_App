e57817f198e17704abd68a00ae1fdeb794221397
Satya1929 <spswain2003.org@gmail.com>
2024-10-30 22:42:38 +0530

    main file

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
 
     st.write(
         """
-        Welcome to the Travel Match App! 🚗 (400+ Users in Diwali with 120+ entries !!)
+        Welcome to the Travel Match App! 🚗 
+        
+        Trusted by 400+ Users in Diwali along with 120+ entries !!
 
         Your go-to platform for finding the perfect travel partner based on shared preferences. Just fill out a quick form and you're all set!
 
